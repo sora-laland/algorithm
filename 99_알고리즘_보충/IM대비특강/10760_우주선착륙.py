f05afad08bb24d3ae2edab67b516c57ca38dc480
soralee <pineapple223663@gmail.com>
2024-02-29 00:05:37 +0900

    0228

--- 99_알고리즘_보충/IM대비특강/10760_우주선착륙.py
+++ 99_알고리즘_보충/IM대비특강/10760_우주선착륙.py
@@ -1,11 +1,12 @@
 import sys; sys.stdin = open("10760.txt")
 
+di = [-1, -1, -1, 0, 0, 1, 1, 1]  # 8방향
+dj = [-1, 0, 1, -1, 1, -1, 0, 1]
+
 T = int(input())
 for t in range(T):
     N, M = map(int, input().split())    # N*M 행렬
     arr = [list(map(int, input().split())) for _ in range(N)]
-    di = [-1, -1, -1, 0, 0, 1, 1, 1]  #8 방향
-    dj = [-1, 0, 1, -1, 1, -1, 0, 1]
     # 8방향을 탐색하면서 타겟보다 값이 작으면 카운트
     # 카운트가 4 이상이면 후보지 카운트
     print(f'#{t+1}', end=' ')
