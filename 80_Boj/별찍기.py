d5cc271a7316ca09d88159d6cf2428b7c5b0b36c
soralee <pineapple223663@gmail.com>
2024-03-04 23:54:22 +0900

    star

--- 80_Boj/별찍기.py
+++ 80_Boj/별찍기.py
@@ -1,11 +1,7 @@
 import sys; sys.stdin = open("input.txt")
 
 N = int(input())
-for i in range(1, N):
-    print('*' * i, end='')
-    print(' ' * (2*(N-i)), end='')
-    print('*' * i)
-for i in range(N-1, -1, -1):
-    print('*' * (i+1), end='')
-    print(' ' * (2*(N-i-1)), end='')
-    print('*' * (i+1))
+print(' ' * (N-1) + '*')
+for i in range(N-1):
+    print(' ' * (N-2-i) + '*', end='')
+    print(' ' * (2*i + 1) + '*')
