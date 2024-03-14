d811b255481a26a71c8b62bd9d680dc0442425da
soralee <pineapple223663@gmail.com>
2024-03-15 00:33:07 +0900

    0314

--- 90_스터디/0314_swea_1949_등산로조성.py
+++ 90_스터디/0314_swea_1949_등산로조성.py
@@ -1,22 +1,19 @@
 import sys; sys.stdin = open("input.txt")
 
-N, K = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
 
 # 일단 k는 고려하지 않고 최장 등산로를 찾는 로직을 생각해보자
 # 봉우리의 좌표는 ti, tj 로 주어진다
 
 di = [0, 0, 1, -1]
 dj = [1, -1, 0, 0]
-st = []
-visited = [[0]*N for _ in range(N)]
-max_cnt = 0
+# max_cnt = 0
+
 
 def find_route(ti, tj):
     global max_cnt
-
+    st = []
+    visited = [[0]*N for _ in range(N)]
     st.append((ti, tj, 1))
-    # cnt = 1
     while st:
         i, j, c = st.pop()
         visited[i][j] = 1
@@ -35,5 +32,41 @@ def find_route(ti, tj):
                 c -= 1
     return max_cnt
 
-a = find_route(0, 1)
-print(a)
+
+T = int(input())
+for t in range(T):
+    N, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    # 봉우리 찾기
+    peaks = []
+    max_height = 0
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] > max_height:
+                max_height = arr[i][j]
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == max_height:
+                peaks.append((i, j))
+    # print(peaks)
+
+    # 각각의 봉우리에서 시작해서
+    # 자신을 제외한 모든 지점을 0~k 만큼 깎아서 경로 구하기
+
+    routes = []
+
+    for peak in peaks:
+        for i in range(N):
+            for j in range(N):
+                # 봉우리가 아닌 곳만 깎는다
+                if i != peak[0] or j != peak[1]:
+                    for k in range(K+1):
+                        max_cnt = 0
+                        arr[i][j] -= k
+                        route = find_route(peak[0], peak[1])
+                        routes.append(route)
+                        arr[i][j] += k      # 다시 더해줌
+
+    print(max(routes))
