145af50d89c3970910cc6bdfe05a8f21effa0123
sora_laland <pineapple223663@gmail.com>
2024-03-19 18:00:41 +0900

    0319-

--- 90_스터디/0315_boj_2644_촌수계산.py
+++ 90_스터디/0315_boj_2644_촌수계산.py
@@ -1,4 +1,5 @@
 import sys; sys.stdin = open("input.txt")
+from collections import deque
 
 N = int(input())
 tarx, tary = map(int, input().split())
@@ -8,4 +9,24 @@ for _ in range(M):
     x, y = map(int, input().split())
     adj[x].append(y)
     adj[y].append(x)
-print(adj)
+# print(adj)
+visited = [0] * (N+1)
+q = deque()
+
+def bfs(start, end):
+    cnt = 0
+    q.append(start)
+    while q:
+        w = q.popleft()
+        visited[w] = 1  # 방문 표시
+        cnt += 1
+        for node in adj[w]:
+            if visited[node]:
+                continue
+            q.append(node)
+            if node == end:
+                return cnt
+    return -1
+
+
+print(bfs(tarx, tary))
