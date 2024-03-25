9bc83610674b0a264531442d65cde5f2d942062a
sora_laland <pineapple223663@gmail.com>
2024-03-25 17:13:00 +0900

    0325

--- 25_문제풀이4_0322/1486_장훈이의높은선반.py
+++ 25_문제풀이4_0322/1486_장훈이의높은선반.py
@@ -1,7 +1,24 @@
 import sys; sys.stdin = open("input.txt")
 
+def dfs(cnt, sum_height):
+    global ans
+    # 기저조건
+    # B 이상이면 종료
+    if sum_height >= B:
+        ans = min(ans, sum_height)
+        return
+    # 모든 점원이 탑을 쌓는데에 고려가 되었다면 종료
+    if cnt == N:
+        return
+    # 쌓는다
+    dfs(cnt+1, sum_height+arr[cnt])
+    # 안쌓는다
+    dfs(cnt+1, sum_height)
+
 T = int(input())
 for t in range(T):
     N, B = map(int, input().split())
     arr = list(map(int, input().split()))
-print()
+    ans = int(1e9)
+    dfs(0, 0)
+    print(f'#{t+1} {abs(ans-B)}')
--- 90_스터디/0325_boj_1987_알파벳.py
+++ 90_스터디/0325_boj_1987_알파벳.py
@@ -1,17 +1,25 @@
 import sys; sys.stdin = open("input.txt")
 
+
 R, C = map(int, input().split())
 arr = [input() for _ in range(R)]
 st = []
 di = [(0, 1), (1, 0), (0, -1), (0, -1)]
-# visited = [[0] * C for _ in range(R)]
+visited = [[0] * C for _ in range(R)]
+max_length = 0
 
-
-st.append((0, 0))
+st.append((0, 0, 1))
 path = ''
 while st:
-    i, j = st.pop()
-    path += arr[i][j]
+    i, j, cnt = st.pop()
+    if cnt < len(path):
+        path = path[:cnt-1]
+    if arr[i][j] not in path:
+        path += arr[i][j]
+
+    if max_length <= len(path):
+        max_length = len(path)
+
     for k in range(4):
         ni = i + di[k][0]
         nj = j + di[k][1]
@@ -19,5 +27,7 @@ while st:
             continue
         if arr[ni][nj] in path:
             continue
-        st.append((ni, nj))
-print(path)
+        st.append((ni, nj, cnt+1))
+
+# print(path)
+print(max_length)
