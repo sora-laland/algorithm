f907428ff06be4eb74ca0e66d5e69f3a7883c40a
sora_laland <pineapple223663@gmail.com>
2024-04-17 17:53:41 +0900

    0417

--- 90_스터디/0421_swea_5656_벽돌깨기.py
+++ 90_스터디/0421_swea_5656_벽돌깨기.py
@@ -1,29 +1,70 @@
 import sys; sys.stdin = open('input.txt')
 from collections import deque
+from itertools import combinations_with_replacement
+from copy import deepcopy
 
-N, W, H = map(int, input().split())
-plate = [list(map(int, input().split())) for _ in range(H)]
 
 di = [-1, 1, 0, 0]
 dj = [0, 0, 1, -1]
 
-def pang(r, c):
+def pang(r, c, arr):
     queue = deque()
     queue.append((r, c))
     while queue:
         i, j = queue.popleft()
-        block = plate[i][j]
-        plate[i][j] = 0
+        block = arr[i][j]
+        arr[i][j] = 0
         for x in range(1, block):
             for k in range(4):
                 ni = i + x * di[k]
                 nj = j + x * dj[k]
                 if 0<=ni<H and 0<=nj<W:
                     queue.append((ni, nj))
+    return arr
 
 
-pang(1, 2)
-print(plate)
+def falling(arr):
+    all_0 = 0
+    for j in range(W):
+        cnt_0 = 0
+        for i in range(H-1, -1, -1):
+            if arr[i][j] == 0:
+                cnt_0 += 1
+            if arr[i][j]:
+                if cnt_0:
+                    arr[i+cnt_0][j] = arr[i][j]
+                    arr[i][j] = 0
+        all_0 += cnt_0
+    return all_0
 
-pang(2, 2)
-print(plate)
+
+def find_top_(j, arr):
+    for i in range(H):
+        if arr[i][j]:
+            return i, j
+    else:
+        return
+
+T = int(input())
+for t in range(T):
+    N, W, H = map(int, input().split())
+    plate = [list(map(int, input().split())) for _ in range(H)]
+
+
+    combis = list(combinations_with_replacement(range(W), N))
+    # W**N 만큼 반복
+    max_0 = 0
+    for combi in combis:
+        arr = deepcopy(plate)
+        # N 만큼 반복
+        for j in combi:
+            top = find_top_(j, arr)
+            if top == None:
+                continue
+            r, c = top
+            pang(r, c, arr)
+            cnt = falling(arr)
+        max_0 = max(max_0, cnt)
+
+    min_block = W*H - max_0
+    print(f'#{t+1} {min_block}')
