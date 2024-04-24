2b1874d5ce5281f5fd03593edd6c3c9afba414d7
soralee <pineapple223663@gmail.com>
2024-04-24 23:27:37 +0900

    0424

--- 80_Boj/N과M/15650_N과M(2).py
+++ 80_Boj/N과M/15650_N과M(2).py
@@ -2,7 +2,6 @@ import sys; sys.stdin = open("input.txt")
 
 N, M = map(int, input().split())
 path = []
-visited = [0] * (N+1)
 
 
 def comb(x):
@@ -11,14 +10,11 @@ def comb(x):
         return
 
     for i in range(1, N+1):
-        if i in path:
-            continue
-        if visited[i] == 1:
+        # 직전에 뽑은 요소가 지금보다 크면 continue
+        if len(path) >= 1 and path[-1] >= i:
             continue
         path.append(i)
-        visited[i] = 1
         comb(x+1)
         path.pop()
-
 
 comb(0)
