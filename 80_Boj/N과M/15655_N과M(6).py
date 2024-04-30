ebff85363574eb3d2b7e338edf211578a5d48d04
sora_laland <pineapple223663@gmail.com>
2024-04-30 17:50:31 +0900

    0430

---
+++ 80_Boj/N과M/15655_N과M(6).py
@@ -0,0 +1,22 @@
+import sys; sys.stdin = open("input.txt")
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+path = []
+
+
+def comb(x):
+    if x == M:
+        print(*path)
+        return
+
+    for i in arr:
+        # 직전에 뽑은 요소가 지금보다 크면 continue
+        if len(path) >= 1 and path[-1] >= i:
+            continue
+        path.append(i)
+        comb(x+1)
+        path.pop()
+
+comb(0)
